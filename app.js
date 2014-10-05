9b6722ef002ecae404c5017c38f4de1d181c79ec
Karen <karenpenglabs@gmail.com>
2014-10-05 11:04:16 -0400

    trying google map

--- app.js
+++ app.js
@@ -29,41 +29,29 @@ var urlencodedParser = bodyParser.urlencoded({
 });
 
 app.get('/', function (req, res) {
-  //res.render('index');
-  //console.log(req.body);
-  //res.send('hello world')
-  //console.log(req.body)
   res.render('index');
 });
 
 // POST / api / users gets JSON bodies
 app.post('/trace', jsonParser, function (req, res) {
   if (!req.body) return res.sendStatus(400);
-  //trace(req.body.data1);
   trace(req, res);
 });
 
 // perform a traceroute:
 function trace(req, res) {
   // get the name of the site to trace:
-  //var pathname = pathname;
   var rawIPs = [];
   var pathname = req.body.siteName;
   // remove the leading /:
   while (pathname.charAt(0) === '/') {
     pathname = pathname.substr(1);
   }
-  //console.log("requested: " + pathname);
-  // write a head to the client, so the browser doesn't complain:
-  // res.writeHead(200, {
-  //   "Content-Type": "text/plain"
-  // });
-  //res.write("Starting trace to " + pathname + "\n\n");
+  console.log("requested: " + pathname);
   console.log("Starting trace to " + pathname + "\n\n");
   // start the trace for real:
   var cmd = exec("traceroute " + pathname, function (error, stdout, stderr) {
     // on completion, close the connection to the client:
-    //res.end("\n\nTrace complete");
     console.log("\n\nTrace complete");
     console.log(rawIPs);
     res.jsonp({
@@ -73,15 +61,7 @@ function trace(req, res) {
 
   // when new data comes in from the trace,pass it to the client:
   cmd.stdout.on('data', function (data) {
-    //res.write(data);
-    //res.send(data);
-    // res.jsonp({
-    //   add: data
-    // });
-    //res.write(data);
     rawIPs.push(data);
-    //console.log(data);
-    //console.lo
   });
 
 }
